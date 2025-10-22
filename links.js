e2d47608c56752b82d4c776b4538c49919af313d
SaraivaGS <saraivagabriel130@gmail.com>
2025-10-22 14:02:40 -0300

    Add game links and details to links.js

---
+++ links.js
@@ -0,0 +1,33 @@
+
+const JOGOS = [
+    { 
+        titulo: "Silent Hill",
+        capa: "silenthillcapa.png",
+        tags: ["Terror", "Survival Horror", "Psicológico", "PT-BR", "PS1"],
+        linkAnuncio: "https://4br.me/SilenthillPs1",
+        linkDireto: "https://drive.google.com/drive/folders/1fM06krUREWRrojI6wxKRcI72y9IaAWCK"
+    },
+    {
+        titulo: "Spider Man",
+        capa: "spidermancapa.png",
+        tags: ["Ação", "Aventura", "Super Herói", "PT-BR", "PS1"],
+        linkAnuncio: "https://fir3.net/Spidermanps1",
+        linkDireto: "https://drive.google.com/file/d/10s7Spl7P3KrkgVXehDkY6lq9AoI_oUJ2/view?usp=drive_link"
+    },
+    {
+        titulo: "Pepsi Man",
+        capa: "pepsimancapa.png",
+        tags: ["Corrida", "Plataforma", "PT-BR", "PS1"],
+        linkAnuncio: "#",
+        linkDireto: "https://drive.google.com/file/d/1bQ-VQUdjhCcDkqmAOrwC8U-48PCaGeVD/view?usp=drive_link"
+    },
+    {
+        titulo: "Tekken 3",
+        capa: "tekken3.png",
+        tags: ["Luta", "Arcade", "PT-BR", "PS1"],
+        linkAnuncio: "#", 
+        linkDireto: "https://drive.google.com/file/d/1FXzMhDBL6_r5RRgCnsLAS16PgAUSzU7Q/view?usp=drive_link"
+    
+    },
+    
+]
